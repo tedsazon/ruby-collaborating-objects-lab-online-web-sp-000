49459b751325d242a22640e45637c16592be4248
Ted Sazon <tedsazon@gmail.com>
2019-01-04 18:28:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
   end
 
   def self.new_by_filename(name)
-    song = name.split(" - ")
+    artist, song = name.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song
