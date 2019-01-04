05e678c2d5049ab984a02dcd7e6565839debea31
Ted Sazon <tedsazon@gmail.com>
2019-01-04 18:13:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -32,7 +32,7 @@ class Artist
 
   def self.find_or_create_by_name(name)
     self.find_by_name(name) ?
-    self.find_by_name(name) : self.create_by_name(name) 
+    self.find_by_name(name) : self.create_by_name(name)
   end
 
 end
